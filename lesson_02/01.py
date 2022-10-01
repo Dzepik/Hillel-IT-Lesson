46600b5c97cfca80a8a23acc8404ad478caad7a0
38067 <dzepik0411@gmail.com>
2022-10-01 13:36:27 +0300

    Исправил домашнее задание 2-го урока!!!!

--- lesson_02/01.py
+++ lesson_02/01.py
@@ -2,14 +2,23 @@
  программа "Касир в кінотеватрі."
 """
 
-age = int(input('Enter you age: '))
+
+while True:
+    age = input('Enter you age, int: ')
+    if age.isdigit():
+        print(f'Thanks for {age}')
+        break
+    else:
+        print(f'{age} - it is not int!')
+age = int(age)
+
 if age < 7:
     print('Де твої батьки?')
 elif 7 < age < 16:
     print('Це фільм для дорослих!')
 elif age > 65:
     print('Покажіть пенсійне посвідчення!')
-elif age == 7 or age == 17 or age == 27 or age == 37 or age == 47 or age == 57 or age == 67 or age == 77 or age == 87 or age == 97:
+elif '7' in str(age):
     print('Вам сьогодні пощастить!')
 else:
     print('А білетів вже немає!')
