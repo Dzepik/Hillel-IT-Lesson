49ae5a96e53994e0acf175ad39e934fe1ed53068
38067 <dzepik0411@gmail.com>
2022-10-09 16:03:20 +0300

    Finish 02

--- lesson_05/02.py
+++ lesson_05/02.py
@@ -5,9 +5,9 @@
 import requests
 from pprint import pprint
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q=dnipro&appid=47503e85fabbabc93cff28c52398ae97&units=metric '
-
-response = requests.get(url).json()
+url = 'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=47503e85fabbabc93cff28c52398ae97&units=metric'
+user_city = input('Enter name city: ')
+response = requests.get(url.format(city_name=user_city)).json()
 pprint(response)
 name_city = response['name']
 speed_wind = response['wind']['speed']
